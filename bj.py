8208100a776c5dc1d66135b959bfe54c93ec9147
pippofantozzi <106916039+pippofantozzi@users.noreply.github.com>
2023-12-22 21:27:10 +0000

    Update bj.py

--- bj.py
+++ bj.py
@@ -28,11 +28,11 @@ if state in double:
 if (state in split) or (your_sum == 12 and has_a):
   split_it = "(Split It! or) "
 
-if state != "":
+if state != "without A +":
   if state in hit:
     st.header(f"{split_it}{dub} Hit It!") 
   else:
-    st.header(f"{split_it}Dont Hit! {state}")
+    st.header(f"{split_it}Dont Hit!")
 
 
 
