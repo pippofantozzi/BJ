94beb35a91c33faeff859821aede546c4274f7bb
pippofantozzi <106916039+pippofantozzi@users.noreply.github.com>
2023-12-22 21:39:26 +0000

    Update bj.py

--- bj.py
+++ bj.py
@@ -32,7 +32,17 @@ if state != " without A + ":
   if state in hit:
     st.header(f"{split_it}{dub} Hit It!") 
   else:
-    st.header(f"{split_it}Dont Hit!")
+    st.header(f"{split_it}Dont Hit!") 
+
+
+st.markdown("---")
+st.title("Some Rules:")
+st.markdown("---")
+st.text("- This method assumes you always play the same initial bet in each hand")
+st.text("- This method assumes you should prioritize splitting over hitting when prompted to do so")
+st.text("- This method assumes that you should only split or double if you have sufficient capital")
+st.text("- This method assumes that if your initial bet is always $10, and you have $6 remaining, you bet $6 (whatever is left)") 
+st.text("Keep in mind: Still not 100% sure about splitting method, but try it for now, except just ignore when it says to split and do whatever is next to it")
 
 
 
