be3a72f0f3659ad33582f0112166b3987f8fe094
pippofantozzi <106916039+pippofantozzi@users.noreply.github.com>
2023-12-27 19:15:15 +0000

    Update bj.py

--- bj.py
+++ bj.py
@@ -2,9 +2,9 @@ import pandas as pd
 import streamlit as st
 from streamlit_option_menu import option_menu
 
-hit = list(pd.read_csv('Hit_If.csv')['Hit If'])
-double = list(pd.read_csv('Double_If.csv')['Double If'])
-split = list(pd.read_csv('Correct_Splits.csv')['Split'])
+hit = list(pd.read_csv('Hit.csv')['State'])
+double = list(pd.read_csv('Double.csv')['State'])
+#split = list(pd.read_csv('Correct_Splits.csv')['Split'])
 
 ## teste
 st.set_page_config(page_title="BJ Houdini", 
@@ -14,21 +14,20 @@ st.set_page_config(page_title="BJ Houdini",
 st.title('Blackjack Houdini')
 your_sum = st.text_input('What is the sum of your cards?:')
 dealers_card = st.text_input('Whats the dealers card?:')
-has_a= st.toggle('Do you have an A?')
+has_a= st.toggle('Do you have a flexible A?')
 if has_a:
-    has_a = 'with A'
+    has_a = 'with flexible A'
 else:
-    has_a = 'without A'
+    has_a = 'without flexible A'
 
 state = f'{your_sum} {has_a} + {dealers_card}' 
 dub = "Just "
 split_it = ""
 if state in double:
   dub = "Double it and "
-if ((state in split) or (your_sum == 12 and has_a)) and int(your_sum) < 20:
-  split_it = "(Split It! or) "
 
-if state != " without A + ":
+
+if (state != " without flexible A + ") or  (state != " with flexible A + "):
   if state in hit:
     st.header(f"{split_it}{dub} Hit It!") 
   else:
